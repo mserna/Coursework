d5fdfb03503ffbdc5add5bef6dd959cae03fd2d5
mserna <mserna@mail.sfsu.edu>
2018-06-14 12:37:53 -0700

    Add notes to main class

--- Blockchain/TestChain.java
+++ Blockchain/TestChain.java
@@ -1,16 +1,25 @@
 /*
 * MUST HAVE PROPS FOR SUCCESSFUL BLOCKCHAIN
 * 1. Decentralizaiton - no single entity is responsible for the data (not one has control of all the processing)
-* 	a. Architectural - 
-*	b. Political - 
-*	c. Logical - 
+* 	a. Architectural - How many nodes are in system and how does it handle fault tolerance
+*	b. Political - How many orgs or peeps control how many nodes
+*	c. Logical - Does interface/data structure look like swarm or single monolithic object 
+*	 	ie. Cut system and half, do both halves continue to run independently
 * 2. Immutability - a write only state (once the data is written, it cannot be updated)
-* 3. 
-* 4. 
+* 
+* Main actors invovled in blockchain:
+* 1. Miners - provide CPU power and act as Gateway to immutable data store. [Can we get rid of this or change?]
+* 2. Users -  Writes and reads to data store. Dependent on miners for CPU power [We need this out to perform dependency]
+
+* Created by Matthew qSerna
+* 6/14/2018
+* TestChain.java
 */
+
 public class TestChain {
 	
 	public static void main(String[] args) {
-		
+		TestChain new_chain = new TestChain();
+		//TODO
 	}
 }
